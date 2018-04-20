2faa7509ba93479e65a8af423bd81309159d0f57
Sam Cuthbertson <samuel.cuthbertson@colorado.edu>
2018-04-20 09:09:06 -0600

    Renaming bad name, both topologies share a class.

--- src/main/java/fun/connor/storm/BothTopology.java
+++ src/main/java/fun/connor/storm/BothTopology.java
@@ -1,8 +1,6 @@
 package fun.connor.storm;
 
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.Properties;
 
 import org.apache.storm.generated.AuthorizationException;
 import org.apache.storm.topology.base.BaseWindowedBolt;
@@ -12,31 +10,24 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import org.apache.storm.Config;
-import org.apache.storm.LocalCluster;
 import org.apache.storm.StormSubmitter;
 import org.apache.storm.generated.AlreadyAliveException;
 import org.apache.storm.generated.InvalidTopologyException;
 import org.apache.storm.topology.TopologyBuilder;
 
-import com.amazonaws.ClientConfiguration;
-import com.amazonaws.regions.Regions;
-import com.amazonaws.services.kinesis.stormspout.InitialPositionInStream;
-import com.amazonaws.services.kinesis.stormspout.KinesisSpout;
-import com.amazonaws.services.kinesis.stormspout.KinesisSpoutConfig;
-
-public class ProcessTopology {
-    private static final Logger LOG = LoggerFactory.getLogger(ProcessTopology.class);
+public class BothTopology {
+    private static final Logger LOG = LoggerFactory.getLogger(BothTopology.class);
 
     public static void main(String[] args) throws IllegalArgumentException, KeeperException, InterruptedException,
             AlreadyAliveException, InvalidTopologyException, IOException {
-        TopologyBuilder rawBuilder = new TopologyBuilder();
+        TopologyBuilder rawBuilder = new TopologyBuilder(); // This topology will process raw tweets
+        TopologyBuilder aveBuilder = new TopologyBuilder(); // This topology will process sorted tweets
 
-        // kafka -> sort_bolt -- ????
+        // kafka -> sort_bolt -- topic is 'raw-tweets'
         rawBuilder.setBolt("sorting_bolt", new SortBolt(), 2).setNumTasks(2).shuffleGrouping("raw_spout");
         // sort_bolt -> kafka (USE REGION FOR TOPIC) I will do this is sorting_bolt -- Sam
 
         // TOPOLOGY SPLIT
-        TopologyBuilder aveBuilder = new TopologyBuilder();
         // kafka regions -> averagebolt w/ direct grouping (will need #num region spouts)
         aveBuilder.setBolt("average_bolt",
                 new AverageBolt().withWindow(BaseWindowedBolt.Duration.minutes(10),
@@ -45,17 +36,26 @@ public class ProcessTopology {
         aveBuilder.setBolt("weather_bolt", new WeatherBolt(), 1).shuffleGrouping("average_bolt").setMemoryLoad(768.0);
         // Annnd weather bolt already outputs to kafka! Yay!
 
-        Config topoConf = new Config();
-        topoConf.setFallBackOnJavaSerialization(true);
-        topoConf.setDebug(true);                 
-        topoConf.setNumEventLoggers(5);          // Arbritrary
-        topoConf.setNumWorkers(10);              // ^
-        topoConf.setMessageTimeoutSecs(1200000); // 20 mins
-        topoConf.registerEventLogger(org.apache.storm.metric.FileBasedEventLogger.class);
+        Config rawConf = new Config();
+        rawConf.setFallBackOnJavaSerialization(true);
+        rawConf.setDebug(true);                 
+        rawConf.setNumEventLoggers(5);          // Arbritrary
+        rawConf.setNumWorkers(10);              // ^
+        rawConf.setMessageTimeoutSecs(1200000); // 20 mins
+        rawConf.registerEventLogger(org.apache.storm.metric.FileBasedEventLogger.class);
+        rawConf.setMaxSpoutPending(5000);
 
-        topoConf.setMaxSpoutPending(5000);
+        Config aveConf = new Config();
+        aveConf.setFallBackOnJavaSerialization(true);
+        aveConf.setDebug(true);                 
+        aveConf.setNumEventLoggers(5);          // Arbritrary
+        aveConf.setNumWorkers(10);              // ^
+        aveConf.registerEventLogger(org.apache.storm.metric.FileBasedEventLogger.class);
+        aveConf.setMaxSpoutPending(5000);
+
         try {
-            StormSubmitter.submitTopology("sorting-topology", topoConf, rawBuilder.createTopology());
+            StormSubmitter.submitTopology("sorting-topology", rawConf, rawBuilder.createTopology());
+            StormSubmitter.submitTopology("average-topology", aveConf, aveBuilder.createTopology());
         } catch (AuthorizationException e) {
             e.printStackTrace();
         }
