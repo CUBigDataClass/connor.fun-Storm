d4bb04c11e04b589da50a96f22b11ad768915017
Sam Cuthbertson <samuel.cuthbertson@colorado.edu>
2018-04-25 12:41:23 -0600

    Saving progress on multilang stuff.

--- src/main/java/fun/connor/storm/BothTopology.java
+++ src/main/java/fun/connor/storm/BothTopology.java
@@ -27,87 +27,102 @@ import org.apache.storm.generated.InvalidTopologyException;
 import org.apache.storm.topology.TopologyBuilder;
 
 public class BothTopology {
-    private static final Logger LOG = LoggerFactory.getLogger(BothTopology.class);
-    private static String zookeeperEndpoint;
-    private static String webserverEndpoint;
-    private static String zookeeperPrefix;
+  private static final Logger LOG = LoggerFactory.getLogger(BothTopology.class);
+  private static String zookeeperEndpoint;
+  private static String webserverEndpoint;
+  private static String zookeeperPrefix;
 
-    public static void main(String[] args) throws IllegalArgumentException, KeeperException, InterruptedException,
-            AlreadyAliveException, InvalidTopologyException, IOException {
-        
-        String propertiesFile = null;
+  public static void main(String[] args)
+      throws IllegalArgumentException, KeeperException, InterruptedException,
+             AlreadyAliveException, InvalidTopologyException, IOException {
 
-        if (args.length != 1) {
-            printUsageAndExit();
-        } else {
-            propertiesFile = args[0];
-        }
+    String propertiesFile = null;
 
-        configure(propertiesFile);
-
-        TopologyBuilder rawBuilder = new TopologyBuilder();
-
-        String topicName = "raw-tweets";
-        BrokerHosts hosts = new ZkHosts(zookeeperEndpoint, "/brokers"); // Assumes Kafka broker uses same zk
-        SpoutConfig spoutConfig = new SpoutConfig(hosts, topicName, "/" + zookeeperPrefix, UUID.randomUUID().toString());
-        spoutConfig.startOffsetTime = OffsetRequest.LatestTime();
-        rawBuilder.setSpout("raw_spout", new KafkaSpout(spoutConfig));
-        
-        rawBuilder.setBolt("sorting_bolt", new SortBolt(webserverEndpoint), 5).shuffleGrouping("raw_spout");
-        rawBuilder.setBolt("sentiment_bolt", new SentimentBolt(), 50).shuffleGrouping("sorting_bolt");
-
-        rawBuilder.setBolt("average_bolt", new AverageBolt().withWindow(BaseWindowedBolt.Duration.minutes(10),
-                        BaseWindowedBolt.Duration.minutes(2)), 100).customGrouping("sentiment_bolt", new RegionGrouping());
-        rawBuilder.setBolt("weather_bolt", new WeatherBolt(), 2).shuffleGrouping("average_bolt").setMemoryLoad(768.0);
-
-        Config rawConf = new Config();
-        rawConf.setFallBackOnJavaSerialization(true);
-        rawConf.setDebug(true);                 
-        rawConf.setNumEventLoggers(5);          // Arbritrary
-        rawConf.setNumWorkers(20);              // ^
-        rawConf.setMessageTimeoutSecs(1400);    // 22 mins
-        rawConf.registerEventLogger(org.apache.storm.metric.FileBasedEventLogger.class);
-        rawConf.setMaxSpoutPending(5000);
-
-        try {
-            StormSubmitter.submitTopology("the-topology", rawConf, rawBuilder.createTopology());
-        } catch (AuthorizationException e) {
-            e.printStackTrace();
-        }
+    if (args.length != 1) {
+      printUsageAndExit();
+    } else {
+      propertiesFile = args[0];
     }
 
-    private static void configure(String propertiesFile) throws IOException {
-        FileInputStream inputStream = new FileInputStream(propertiesFile);
-        Properties properties = new Properties();
-        try {
-            properties.load(inputStream);
-        } finally {
-            inputStream.close();
-        }
+    configure(propertiesFile);
 
-        String zookeeperEndpointOverride = properties.getProperty("zookeeperEndpoint");
-        if (zookeeperEndpointOverride != null) {
-            zookeeperEndpoint = zookeeperEndpointOverride;
-        }
-        LOG.info("Using zookeeper endpoint " + zookeeperEndpoint);
+    TopologyBuilder rawBuilder = new TopologyBuilder();
 
-        String zookeeperPrefixOverride = properties.getProperty("zookeeperPrefix");
-        if (zookeeperPrefixOverride != null) {
-            zookeeperPrefix = zookeeperPrefixOverride;
-        }
-        LOG.info("Using zookeeper prefix " + zookeeperPrefix);
+    String topicName = "raw-tweets";
+    BrokerHosts hosts = new ZkHosts(
+        zookeeperEndpoint, "/brokers"); // Assumes Kafka broker uses same zk
+    SpoutConfig spoutConfig = new SpoutConfig(
+        hosts, topicName, "/" + zookeeperPrefix, UUID.randomUUID().toString());
+    spoutConfig.startOffsetTime = OffsetRequest.LatestTime();
+    rawBuilder.setSpout("raw_spout", new KafkaSpout(spoutConfig));
 
-        String webserverEndpointOverride = properties.getProperty("webserverEndpoint");
-        if (webserverEndpointOverride != null) {
-            webserverEndpoint = webserverEndpointOverride;
-        }
-        LOG.info("Using webserver endpoint " + webserverEndpoint);
+    rawBuilder.setBolt("sorting_bolt", new SortBolt(webserverEndpoint), 5)
+        .shuffleGrouping("raw_spout");
+    rawBuilder.setBolt("sentiment_bolt", new SentimentBolt(), 50)
+        .shuffleGrouping("sorting_bolt");
 
+    rawBuilder
+        .setBolt(
+            "average_bolt",
+            new AverageBolt().withWindow(BaseWindowedBolt.Duration.minutes(10),
+                                         BaseWindowedBolt.Duration.minutes(2)),
+            100)
+        .customGrouping("sentiment_bolt", new RegionGrouping());
+    rawBuilder.setBolt("weather_bolt", new WeatherBolt(), 2)
+        .shuffleGrouping("average_bolt")
+        .setMemoryLoad(768.0);
+
+    Config rawConf = new Config();
+    rawConf.setFallBackOnJavaSerialization(true);
+    rawConf.setDebug(true);
+    rawConf.setNumEventLoggers(5);       // Arbritrary
+    rawConf.setNumWorkers(20);           // ^
+    rawConf.setMessageTimeoutSecs(1400); // 22 mins
+    rawConf.registerEventLogger(
+        org.apache.storm.metric.FileBasedEventLogger.class);
+    rawConf.setMaxSpoutPending(5000);
+
+    try {
+      StormSubmitter.submitTopology("the-topology", rawConf,
+                                    rawBuilder.createTopology());
+    } catch (AuthorizationException e) {
+      e.printStackTrace();
+    }
+  }
+
+  private static void configure(String propertiesFile) throws IOException {
+    FileInputStream inputStream = new FileInputStream(propertiesFile);
+    Properties properties = new Properties();
+    try {
+      properties.load(inputStream);
+    } finally {
+      inputStream.close();
     }
 
-    private static void printUsageAndExit() {
-        System.out.println("Usage: " + BothTopology.class.getName() + " <propertiesFile>");
-        System.exit(-1);
+    String zookeeperEndpointOverride =
+        properties.getProperty("zookeeperEndpoint");
+    if (zookeeperEndpointOverride != null) {
+      zookeeperEndpoint = zookeeperEndpointOverride;
     }
+    LOG.info("Using zookeeper endpoint " + zookeeperEndpoint);
 
+    String zookeeperPrefixOverride = properties.getProperty("zookeeperPrefix");
+    if (zookeeperPrefixOverride != null) {
+      zookeeperPrefix = zookeeperPrefixOverride;
+    }
+    LOG.info("Using zookeeper prefix " + zookeeperPrefix);
+
+    String webserverEndpointOverride =
+        properties.getProperty("webserverEndpoint");
+    if (webserverEndpointOverride != null) {
+      webserverEndpoint = webserverEndpointOverride;
+    }
+    LOG.info("Using webserver endpoint " + webserverEndpoint);
+  }
+
+  private static void printUsageAndExit() {
+    System.out.println("Usage: " + BothTopology.class.getName() +
+                       " <propertiesFile>");
+    System.exit(-1);
+  }
 }
