7d909ac8dac79135f40c7af91ce9314309208ae1
Sam Cuthbertson <samuel.cuthbertson@colorado.edu>
2018-04-25 12:41:23 -0600

    Saving progress on multilang stuff.

--- multilang/resources/sentiment.py
+++ multilang/resources/sentiment.py
@@ -1,15 +1,21 @@
 import storm
-import boto3
+import numpy
+import sklearn
 
-class SentimentBolt(storm.BasicBolt):
+class SentimentBolt(storm.Bolt):
     def initialize(self, conf, context):
-        self.comp = boto3.client(service_name='comprehend', region_name='us-west-2')
+        # Load pickle
+        self._conf = conf
+        self._context = context
+        #self.comp = boto3.client(service_name='comprehend', region_name='us-west-2')
 
     def process(self, tup):
+        #import sys
         text = tup.values[1]
-        sentiment = self.comp.detect_sentiment(Text=text, LanguageCode='en')
+        #sentiment = self.comp.detect_sentiment(Text=text, LanguageCode='en')
 
-        score = sentiment["SentimentScore"]["Positive"] - sentiment["SentimentScore"]["Negative"]
+        # Classify
+        score = 0.01 #sentiment["SentimentScore"]["Positive"] - sentiment["SentimentScore"]["Negative"]
         storm.logInfo("SentimentBolt got: " + str(score) + " for text " + text)
 
         tup.values[1] = score
